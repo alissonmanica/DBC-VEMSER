3118241538544890dee07335967b0193422a0a3d
Alisson Manica <alissonmanica.aai@gmail.com>
2022-03-31 11:39:17 -0300

    fix: pagina Address.

--- ReactJS/Exercicios/aula06/src/pages/address/Address.js
+++ ReactJS/Exercicios/aula06/src/pages/address/Address.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup' 
 
 import { AuthContext } from '../../context/AuthContext';
 import apiCep from '../../components/apiCep';
@@ -15,22 +16,30 @@ const [address, setAddress] = useState({})
 
 const {localidade, logradouro, bairro, ddd, uf} = address
 
+function maskCEP(value) {
+  return value.replace(/\D/g, "").replace(/^(\d{5})(\d{3})+?$/, "$1-$2");
+};
+
+
+function callCep(values) {
+    values.cep = maskCEP(cep)
+    values.rua = logradouro;
+    values.bairro = bairro
+    values.cidade = localidade
+    values.estado = uf
+    values.ddd = ddd
+}
 
 async function getCep({values}) {
   try {
     const {data} = await apiCep.get(`/${cep}/json/`)
     setAddress(data)
-      console.log(values)
-        values.rua = logradouro;
-        values.bairro = bairro
-        values.cidade = localidade
-        values.estado = uf
-        values.ddd = ddd
+    callCep(values)
     } catch (error) {
       console.log(error)
     }
-    console.log(address)
   }
+
 
 
   function Setup() {
@@ -43,21 +52,16 @@ async function getCep({values}) {
     Setup()
   },[])
 
-  function verifyValues(values) {
-    if(values.cep === '' && values.rua === '' && values.bairro === '' && values.cidade === '' && values.estado === '' && values.ddd === '' && values.telefone === '') {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
-  function verifyObject() {
-    if (Object.keys(address).length) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  const formSchema = Yup.object().shape({
+    cep: Yup.string().min(8, 'Campo tem que conter 8 digitos').max(9, 'Campo só pode conter 8 digitos').required('Campo obrigatório'),
+    rua: Yup.string().required('Campo obrigatório'),
+    bairro: Yup.string().required('Campo obrigatório'),
+    cidade: Yup.string().required('Campo obrigatório'),
+    estado: Yup.string().required('Campo obrigatório'),
+    ddd: Yup.string().min(2, 'Campo tem que conter 2 digitos').max(2, 'Campo só pode conter 2 digitos').required('Campo obrigatório'),
+    telefone: Yup.string().min(8, 'Campo tem que conter 8 digitos').max(9, 'Campo só pode conter 8 digitos').required('Campo obrigatório'),
+  });
 
   return (
     <div className={styles.address}>
@@ -73,26 +77,24 @@ async function getCep({values}) {
         ddd: '',
         telefone: '',
       }}
+      validationSchema={formSchema}
       onSubmit={(values) => {
-        if (verifyObject() && !verifyValues(values) ) {
-          alert(JSON.stringify(values, null, 2));
-          console.log(values)
-        } else {
-          alert('error!')
-        }
+        alert(JSON.stringify(values, null, 2));
       }}
     >
       {values => (
       <Form>
         <div>
           <label htmlFor="cep">CEP: </label>
-          <Field id="cep" name="cep" placeholder="Digite seu CEP" onBlur={(e) => setCep(e.target.value)} />
+          <Field id="cep" name="cep" placeholder="Digite seu CEP" value={maskCEP(cep)} onChange={(e) => setCep(e.target.value)}/>
           <button type="button" onClick={() => getCep(values)}>Buscar CEP</button>
+          <ErrorMessage name='cep' />
         </div>
 
         <div>
           <label htmlFor="rua">Rua: </label>
           <Field id="rua" name="rua" placeholder="Digite sua Rua" />
+          <ErrorMessage name='rua' />
         </div>
 
         <div>
@@ -103,26 +105,31 @@ async function getCep({values}) {
         <div>
           <label htmlFor="bairro">Bairro: </label>
           <Field id="bairro" name="bairro" placeholder="Digite seu Bairro" />
+          <ErrorMessage name='bairro' />
         </div>
 
         <div>
           <label htmlFor="cidade">Cidade: </label>
           <Field id="cidade" name="cidade" placeholder="Digite sua Cidade" />
+          <ErrorMessage name='cidade' />
         </div>
 
         <div>
           <label htmlFor="estado">Estado: </label>
           <Field id="estado" name="estado" placeholder="Digite seu Estado" />
+          <ErrorMessage name='cidade' />
         </div>
 
         <div>
           <label htmlFor="ddd">DDD: </label>
           <Field id="ddd" name="ddd" placeholder="Digite o DDD" />
+          <ErrorMessage name='ddd' />
         </div>
 
         <div>
           <label htmlFor="telefone">Telefone: </label>
           <Field id="telefone" name="telefone" placeholder="Digite seu Telefone" />  
+          <ErrorMessage name='telefone' />
         </div>
 
         <button type="submit">Enviar</button>
